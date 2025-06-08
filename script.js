130f25421b2acfa45e72cadfbedaf20ea70fbff3
esimp222 <elizabethsimpson96@outlook.com>
2025-06-08 15:19:21 +0100

    colour updates and styling changes

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const colorDisplay = document.querySelector(".color-display");
 const playlist = document.querySelector(".playlist");
 const title = document.querySelector(".title")
 
-// Predefined colors and playlists
+// predefined colors and playlists
 const moodData = {
     happy: { colour: ["#FAE791", "#F28DBF"], playlist: "https://open.spotify.com/embed/playlist/37i9dQZF1DX3rxVfibe1L0" },
-    calm: { colour: ["#ADD8E6", "#5E9167"], playlist: "https://open.spotify.com/embed/playlist/37i9dQZF1DX4sWSpwq3LiO" },
+    calm: { colour: ["#ffffff", "#5E9167"], playlist: "https://open.spotify.com/embed/playlist/37i9dQZF1DX4sWSpwq3LiO" },
     energetic: { colour: ["#FF4500","#51DB42"], playlist: "https://open.spotify.com/embed/playlist/37i9dQZF1EIeEZPgsd7pko" },
-    sad: { colour: ["#708090", "#E7F2E6"], playlist: "https://open.spotify.com/embed/playlist/37i9dQZF1DX3YSRoSdA634" }
+    sad: { colour: ["#000000", "#E7F2E6"], playlist: "https://open.spotify.com/embed/playlist/37i9dQZF1DX3YSRoSdA634" }
 };
 
-// Change background and display playlist
+// change background and display playlist
 moodSelector.addEventListener("change", () => {
     const mood = moodSelector.value;
     const colours = moodData[mood].colour;
